7ebf8e0b65808cc9e3f324e13a98c1fe5d502ebe
rampadam1 <91225884+rampadam1@users.noreply.github.com>
2021-12-08 16:28:17 -0500

    New stuff

--- ScriptFile.js
+++ ScriptFile.js
@@ -1,20 +1,26 @@
-document.addEventListener("keyup", movementKey, false);
-var elem = document.getElementById('box');
+
+
+var canvas = document.createElement("canvas");
+var b = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 400;
+
+document.body.appendChild(canvas)
+
+b.fillStyle = "#FF0000";
+b.fillRect(0,0,canvas.width, canvas.height);
+
+
+var posX = 200;
+var posY = 200;
+
+b.fillStyle = "white";
+b.fillRect(posX,posY, 30,40) 
+
+
 
 function movementKey(event){
-    var key = event.key.toLowerCase();
-
-    if(key == "w"){
-        elem.style.position.top+=3;
-    }
-    if(key == "S"){
-        elem.style.position.down+=3;
-    }
-    if(key == "A"){
-        elem.style.position.left+=3;
-    }
-    if(key == "D"){
-        elem.style.position.right+=3;
-    }
+    
 
 }
