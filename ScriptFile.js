b5989ecbebfba5d875616971e8f78066dccc9807
rampadam1 <91225884+rampadam1@users.noreply.github.com>
2021-12-08 16:38:34 -0500

    Movement update

--- ScriptFile.js
+++ ScriptFile.js
@@ -1,5 +1,5 @@
 
-
+document.addEventListener("keydown", movementKey, true);
 var canvas = document.createElement("canvas");
 var b = canvas.getContext("2d");
 
@@ -21,6 +21,20 @@ b.fillRect(posX,posY, 30,40)
 
 
 function movementKey(event){
+    console.log(event.key);
     
+    if(event.key == "w"){posY-=5}
+    if(event.key == "s"){posY+=5}
+    if(event.key == "a"){posX-=5}
+    if(event.key == "d"){posX+=5}
+
+b.fillStyle = "#FF0000";
+b.fillRect(0,0,canvas.width, canvas.height);
+
+
+
+b.fillStyle = "white";
+b.fillRect(posX,posY, 30,40) 
+
 
 }
